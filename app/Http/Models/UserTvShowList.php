337b87e98bdb2d5b847eaa7c9ca4f1cea3c21966
alid404 <chainsawman.11294@hotmail.com>
2025-04-07 19:15:40 +0100

    Upload of the User tv Show list Model

---
+++ app/Http/Models/UserTvShowList.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserTvShowList extends Model
+{
+    use HasFactory;
+
+    protected $table = 'user_tv_show_list'; 
+
+    protected $fillable = ['user_id', 'tmdb_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
